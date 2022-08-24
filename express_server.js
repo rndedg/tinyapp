c9855c07bafcfb75813e1a58a74341cb2f9b61c1
Gord Letkeman <rndedg@gmail.com>
2022-08-24 16:07:55 +0000

    Made changes to login post to respond with an error 400 for blank fields.

--- express_server.js
+++ express_server.js
@@ -1,12 +1,10 @@
 // Implement generateRandomString function
-
 const generateRandomString = () => {
   const result = Math.random().toString(36).substring(2,7);
   return result;
 };
 
 // Implement userLookup function
-
 const getUserByEmail = (email) => {
   for (const user in users) {
     if (users[user].email === email) {
@@ -19,7 +17,6 @@ const getUserByEmail = (email) => {
 
 
 // Setting required functions and port variables
-
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
@@ -52,7 +49,6 @@ app.use(cookieParser());
 
 
 // GET Routes
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -94,7 +90,6 @@ app.get("/register", (req, res) => {
 
 
 // Add Login get route
-
 app.get("/login", (req, res) => {
   const templateVars = { urls: urlDatabase, user: users[req.cookies.user_id] };
   res.render("urls_login", templateVars);
@@ -113,7 +108,6 @@ app.post("/urls", (req, res) => {
 });
 
 // Add POST route to handle Delete functionality
-
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   delete urlDatabase[req.body.longURL];
@@ -121,7 +115,6 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 // Add POST route to handle Edit functionality
-
 app.post("/urls/:id/edit", (req, res) => {
   urlDatabase[req.params.id] = req.body.updatedURL;
   res.redirect("/urls");
@@ -129,11 +122,12 @@ app.post("/urls/:id/edit", (req, res) => {
 
 
 // Add POST route for cookie handling
-
 app.post("/login", (req, res) => {
   const userLogin = getUserByEmail(req.body.email);
 
-  if (!userLogin) {
+  if (req.body.email.length <= 0 && req.body.password.length <= 0) {
+    res.status(400).send("Please enter e-mail and password.");
+  } else if (!userLogin) {
     res.status(403).send("E-mail not found.");
   } else if (req.body.password !== userLogin.password) {
     res.status(403).send("Incorrect password.");
@@ -145,14 +139,12 @@ app.post("/login", (req, res) => {
 });
 
 // Add POST route to handling Logging out
-
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
 // Add POST route to register new user
-
 app.post("/register", (req, res) => {
 
   if (!req.body.email.length) {
@@ -170,7 +162,6 @@ app.post("/register", (req, res) => {
 });
 
 // Set server to listen
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
