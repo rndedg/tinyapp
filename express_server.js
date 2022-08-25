f29ef062bac7f6c532eac855141952f1cf5bb6c5
Gord Letkeman <rndedg@gmail.com>
2022-08-25 20:40:05 +0000

    Add user permissions

--- express_server.js
+++ express_server.js
@@ -14,6 +14,19 @@ const getUserByEmail = (email) => {
   return null;
 };
 
+// Implement urlsForUser(id) which returns the URLs where the userID is equal to the id of the currently logged-in user.
+const urlsForUser = (userIdent) => {
+  let usersUrls = {};
+  for (const shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === userIdent.id) {
+      usersUrls[shortURL] = urlDatabase[shortURL];
+    }
+  }
+  return usersUrls;
+  
+};
+
+
 
 
 // Setting required functions and port variables
@@ -24,10 +37,23 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+// Original DB
+// const urlDatabase = {
+//   "b2xVn2": "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com"
+// };
+
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
 };
+
 
 const users = {
   userRandomID: {
@@ -63,55 +89,112 @@ app.get("/hello", (req, res) => {
 
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, user: users[req.cookies.user_id] };
+  const userID = users[req.cookies.user_id];
+  if (!userID) {
+    return res.status(401).send("Error 401. You must be logged in to do that.");
+  }
+  const usersUrls = urlsForUser(userID);
+  const templateVars = { urls: usersUrls, user: userID };
   res.render("urls_index", templateVars);
 });
 
+
 app.get("/urls/new", (req, res) => {
-  const templateVars = { urls: urlDatabase, user: users[req.cookies.user_id] };
-  res.render("urls_new", templateVars);
+  const usersUrls = urlsForUser(users[req.cookies.user_id]);
+  const templateVars = { urls: usersUrls, user: users[req.cookies.user_id] };
+
+  if (!users[req.cookies.user_id]) {
+    res.redirect("/login");
+  } else {
+    res.render("urls_new", templateVars);
+  }
 });
 
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: users[req.cookies.user_id] };
-  res.render("urls_show", templateVars);
+  const userID = users[req.cookies.user_id];
+  const shortURL = req.params.id;
+
+  if (!urlDatabase[shortURL]) {
+    return res.status(404).send("Error 404. Short URL not found.");
+  }
+
+  const usersUrls = urlsForUser(users[req.cookies.user_id]);
+  if (!userID || !usersUrls[shortURL]) {
+    return res.status(401).send("Error 401. You do not have permission to see this URL.");
+  }
+
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL, user: userID };
+  if (!userID) {
+    res.status(401).send("Error 401. You must be logged in to do that.");
+  } else {
+    res.render("urls_show", templateVars);
+  }
 });
 
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
+  const longURL = urlDatabase[req.params.id].longURL;
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.status(404).send("Error 404. This short URL is not in the database.");
+  }
 });
 
 app.get("/register", (req, res) => {
   const templateVars = { urls: urlDatabase, user: users[req.cookies.user_id] };
-  res.render("register", templateVars);
+
+  if (users[req.cookies.user_id]) {
+    res.redirect("/urls");
+  } else {
+    res.render("register", templateVars);
+  }
 });
 
 
 // Add Login get route
 app.get("/login", (req, res) => {
-  const templateVars = { urls: urlDatabase, user: users[req.cookies.user_id] };
-  res.render("urls_login", templateVars);
+  const userID = users[req.cookies.user_id];
+  const templateVars = { urls: urlDatabase, user: userID};
+  if (users[userID]) {
+    res.redirect("/urls");
+  } else {
+    res.render("urls_login", templateVars);
+  }
 });
 
 
 
 // POST Routes
-
-
+//
 // Generate random shortURL and redirect to show results
 app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(302, `/urls/${shortURL}`);
+  if (users[req.cookies.user_id]) {
+    const shortURL = generateRandomString();
+    urlDatabase[shortURL] = {
+      longURL: req.body.longURL,
+      userID: req.cookies.user_id
+    };
+    res.redirect(302, `/urls/${shortURL}`);
+  } else {
+    res.status(401).send("Error 401. You must be logged in to do that.");
+  }
 });
 
 // Add POST route to handle Delete functionality
 app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id];
-  delete urlDatabase[req.body.longURL];
-  res.redirect("/urls/");
+  const userID = users[req.cookies.user_id];
+  const shortURL = req.params.id;
+
+  if (!urlDatabase[shortURL])  {
+    res.status(404).send("URL does not exsist.");
+  } else if (userID === urlDatabase[shortURL].userID || userID) {
+    delete urlDatabase[req.params.id];
+    delete urlDatabase[req.body.longURL];
+    res.redirect("/urls/");
+  } else {
+    res.status(401).send("You are not authorized to do that.");
+  }
 });
 
 // Add POST route to handle Edit functionality
@@ -141,7 +224,7 @@ app.post("/login", (req, res) => {
 // Add POST route to handling Logging out
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
-  res.redirect("/urls");
+  res.redirect("/login");
 });
 
 // Add POST route to register new user
@@ -165,6 +248,3 @@ app.post("/register", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-
-///CLEAN
